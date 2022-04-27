5d2c23934cac55cc63771fb87df163272f395a3e
Matheus <matheusfoaraujo@gmail.com>
2022-04-27 13:25:02 -0300

    adds requirement-2 codes

--- src/App.js
+++ src/App.js
@@ -11,16 +11,19 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
+      <section>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
 
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="/search" component={ Search } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="*" component={ NotFound } />
-      </Switch>
+      </section>
 
     // :code os dois pontos faz com que code seja visto como um apelido pro que tiver dentro de details
     // exact significa que tem que ser exatamente só "/" pra começar na home
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,100 @@
+/* import propTypes from 'prop-types'; */
 import React, { Component } from 'react';
-// import { createUser } from '../services/userAPI';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      login: '',
+      isEnterButtonDisabled: true,
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  /*   componentDidMount() {
+    this.onEnterButtonClick();
+  } */
+
+  /*   componentWillUnmount() {
+    this.onEnterButtonClick();
+  } */
+
+  onInputChange = ({ target }) => { // desconstroi o evento pegando o target, o node que está sendo modificado
+    const { value } = target; // desconstroi o valor de dentro do target que está sendo modificado
+    this.setState({ login: value }, () => {
+      // callback que pega os valores do estado já atualizados pelo onChange
+      const { login } = this.state;
+      console.log(this.state);
+      const minChar = 2;
+      if (login.length > minChar) {
+        this.setState({ isEnterButtonDisabled: false });
+      } else {
+        this.setState({ isEnterButtonDisabled: true });
+      }
+    });
+  }
+
+  onEnterButtonClick = async () => {
+    const { login } = this.state;
+
+    this.setState({
+      loading: true, // faz com que loading seja true e mostre a mensagem de loading enquanto o CreateUser não for chamado.
+
+    });
+    await createUser({ name: login });
+
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  }
+
   render() {
+    const { login,
+      isEnterButtonDisabled, loading, redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/search" />;
+    }
     return (
-      <>
-        <div>
-          <div data-testid="page-login">Login</div>
-        </div>
-        <div>
-          <form action="">
-            <input type="text" data-testid="login-name-input" />
+      <section>
+        {loading ? (<Loading />) : (
 
-            <button type="button" data-testid="login-submit-button">Entrar</button>
-          </form>
-        </div>
+          <>
+            <div data-testid="page-login">Login</div>
+            <div>
+              <form>
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  value={ login }
+                  onChange={ this.onInputChange }
+                />
 
-      </>
+                <button
+                  type="button"
+                  data-testid="login-submit-button"
+                  disabled={ isEnterButtonDisabled }
+                  onClick={ this.onEnterButtonClick }
+                >
+                  Entrar
+                </button>
+              </form>
+            </div>
+
+          </>
+        )}
+      </section>
+
     );
   }
 }
+
+/* Login.propTypes = {
+  login: propTypes.string.isRequired,
+  onInputChange: propTypes.func.isRequired,
+  isEnterButtonDisabled: propTypes.bool.isRequired,
+}; */
