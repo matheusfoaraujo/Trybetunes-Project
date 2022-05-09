771fb015284267a9e58ab21da46cdf1800aa0dfc
Matheus <matheusfoaraujo@gmail.com>
2022-05-08 21:37:17 -0300

    requirement 4 completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -26,9 +27,41 @@ export default class Header extends Component {
         {loading ? (<Loading />) : (
 
           <header data-testid="header-component">
-            <h2 data-testid="header-user-name">
-              {loggedUser.name}
-            </h2>
+            <div>
+              <Link
+                to="/search"
+                data-testid="link-to-search"
+                className="search button"
+              >
+                Search
+
+              </Link>
+            </div>
+            <div>
+              <Link
+                to="/favorites"
+                data-testid="link-to-favorites"
+                className="search button"
+              >
+                Favorites
+
+              </Link>
+            </div>
+            <div>
+              <Link
+                to="/profile"
+                data-testid="link-to-profile"
+                className="search button"
+              >
+                Profile
+
+              </Link>
+            </div>
+            <div>
+              <h2 data-testid="header-user-name">
+                {loggedUser.name}
+              </h2>
+            </div>
           </header>
         )}
       </div>
