9d58bca64fc7447817a81909bc671178c8424cf3
Matheus <matheusfoaraujo@gmail.com>
2022-05-12 00:45:21 -0300

    requirement 13 completed

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends Component {
+  state = {
+    favesongs: [],
+  }
+
+  componentDidMount = async () => {
+    const faveSongsResponse = await getFavoriteSongs();
+    this.setState({ favesongs: faveSongsResponse });
+  }
+
   render() {
+    const { favesongs } = this.state;
     return (
       <section>
         <div>
           <Header />
         </div>
-        <div data-testid="page-favorites">Favorites</div>
+        <div data-testid="page-favorites">
+          {favesongs.length >= 1 ? favesongs.map((music, index) => (
+            <div key={ index }>
+              {music.trackName
+                ? (
+                  <div>
+                    <MusicCard
+                      song={ music }
+                    />
+
+                  </div>)
+                : ''}
+            </div>
+          ))
+            : ''}
+        </div>
       </section>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Profile extends Component {
-  render() {
-    return (
-      <section>
-        <div>
-          <Header />
-        </div>
-        <div data-testid="page-profile">Profile</div>
-      </section>
-    );
-  }
+state = {
+  userProfile: {},
+  loading: false,
+}
+
+componentDidMount = async () => {
+  this.setState({
+    loading: true,
+  });
+  const user = await getUser();
+  this.setState({
+    userProfile: user,
+    loading: false,
+  });
+  console.log(user);
+}
+
+render() {
+  const { loading, userProfile } = this.state;
+  return (
+    <section>
+      <div>
+        <Header />
+      </div>
+      <div data-testid="page-profile">
+        {loading ? <Loading /> : (
+          <>
+            <>
+              <p>{userProfile.name}</p>
+              <p>{userProfile.email}</p>
+              <img
+                data-testid="profile-image"
+                src={ userProfile.image }
+                alt="profileImage"
+              />
+              <p>{userProfile.description}</p>
+            </>
+            <Link to="/profile/edit">Editar perfil</Link>
+
+          </>
+        )}
+      </div>
+    </section>
+  );
+}
 }
